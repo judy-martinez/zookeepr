48c79dbd57835421cf53a74de9edd89b0a56d088
Judy <judy.mtz1@gmail.com>
2022-03-12 00:59:03 -0600

    access query property on the req object

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const { animals } = require('./data/animals');
 const app = express();
 
 app.get('/api/animals', (req, res) => {
-    res.send(animals);
-});
+    let results = animals;
+    console.log(req.query)
+    res.json(results);
+  });
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
